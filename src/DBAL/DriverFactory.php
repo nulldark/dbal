c755f03adab9d78cf334e12560c03b764b99405e
Dominik Szamburski <dominikszamburski99@gmail.com>
2023-10-13 06:01:57 +0200

    feat: create new driver instance via factory

---
+++ src/DBAL/DriverFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Copyright (C) 2023 Dominik Szamburski
+ *
+ * This file is part of nulldark/dbal
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+namespace Nulldark\DBAL;
+
+use Nulldark\DBAL\Contract\DriverFactoryInterface;
+use Nulldark\DBAL\Contract\DriverInterface;
+use Nulldark\DBAL\Database\InMemory\InMemoryDriver;
+use Nulldark\DBAL\Database\MySQL\MySQLDriver;
+use Nulldark\DBAL\Database\Postgres\PostgresDriver;
+use Nulldark\DBAL\Exception\UnsupportedDriverException;
+
+/**
+ * @author Dominik Szamburski
+ * @package DBAL
+ * @license LGPL-2.1
+ * @version 0.3.0
+ */
+class DriverFactory implements DriverFactoryInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function createDriver(#[\SensitiveParameter] array $params): DriverInterface
+    {
+        return match ($params['driver']) {
+            'inmemory' => new InMemoryDriver(),
+            'mysql' => new MySQLDriver(),
+            'pgsql' => new PostgresDriver(),
+            default => throw new UnsupportedDriverException()
+        };
+    }
+}
